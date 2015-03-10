4b5a8b02f13c8f548f8de94c86dd396e19635c0d
João Santos <joao.santos@zalando.de>
2015-03-10 15:38:19 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='GitConfig',
+    packages=['gitconfig'],
+    version=1,
+    description='Git Config Wrapper',
+    author='Zalando',
+    author_email='joao.santos@zalando.de',
+    url='https://zalando.de',  # TODO replace with real repository
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Version Control',
+    ],
+    long_description=open('README.md').read(),
+)
+
